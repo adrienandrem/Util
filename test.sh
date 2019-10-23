42cec37671fa51a0cdde123be7afcc88bff2b3fb
Adrien André <adr.andre@laposte.net>
2019-10-23 10:44:25 -0400

    Format testing script

--- test.sh
+++ test.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
 JUNIT=/usr/share/java/junit.jar
 test -r "$JUNIT" || JUNIT=/usr/share/junit/lib/junit.jar
-javac -g src/com/hughes/util/*.java src/com/hughes/util/raf/*.java test/com/hughes/util/*.java test/com/hughes/util/raf/*.java -classpath "$JUNIT"
+
+# Compilation
+javac -g \
+  src/com/hughes/util/*.java \
+  src/com/hughes/util/raf/*.java \
+  test/com/hughes/util/*.java \
+  test/com/hughes/util/raf/*.java \
+  -classpath "$JUNIT"
+
+# Tests
 java -classpath "src:test:$JUNIT" junit.textui.TestRunner com.hughes.util.StringUtilTest
 java -classpath "src:test:$JUNIT" junit.textui.TestRunner com.hughes.util.raf.UniformRAFListTest
 java -classpath "src:test:$JUNIT" junit.textui.TestRunner com.hughes.util.raf.RAFListTest
